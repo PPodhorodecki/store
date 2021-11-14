791202a1324470c1c58ce00d0af0314f20e710e3
Emek1902 <autowrapper@gmail.com>
2021-11-14 17:01:31 +0100

    add templates to product and products, actualization urls, views settings

--- config/urls.py
+++ config/urls.py
@@ -18,12 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
 
-from projects.store.config import settings
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('products', include('sklep.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
+
 ]
 
 if settings.DEBUG:
--- sklep/views.py
+++ sklep/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from sklep.models import Product
+
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'sklep/products.html', {'products': products})
+
+def product(request, pk):
+    item = get_object_or_404(Product, pk=pk)
+
+    return render(request, 'sklep/product.html' ,{'product': item})
